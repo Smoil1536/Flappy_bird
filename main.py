864f234b9af3a8ced6784fa1ec0b0ea65fcaf1a3
Smoil <neetubar450@gmail.com>
2025-03-18 18:00:24 +0530

    Added window and stopped instant exit

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 import pygame as pg
 
+def set_game_icon():
+    game_icon = pg.image.load("assets/favicon.ico")
+    pg.display.set_icon(game_icon)
+    
 pg.init()
+window = pg.display.set_mode((600, 600))
+set_game_icon()
+
+running = True
+while running:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            print(event.type)
+            running = False
