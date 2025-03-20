6e16c0896bebfee40de13377b1c84ea38ab405c1
Smoil <neetubar450@gmail.com>
2025-03-20 19:23:27 +0530

    Fixed the movement of pipes

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ load_images()
 width = bg_image.get_size()[0]
 height = bg_image.get_size()[1]
 
+pipe_width = pipe_image.get_size()[0]
 pipe_height = pipe_image.get_size()[1]
 
 bird_width = bird_image_mid.get_size()[0]
@@ -42,46 +43,52 @@ Flappy_bird.y = (height/2) - (bird_height/2) # 12 is half of height of bird
 Beam_flipped = pipe.Pipe()
 Beam_flipped.y = pipe.calc_coords()
 Beam = pipe.Pipe()
-Beam.y = (Beam_flipped.y+pipe_height) + (bird_height*3)
+Beam.y = (Beam_flipped.y+pipe_height) + (bird_height*4)
 
 # Init pipe 2
 Beam_flipped_2 = pipe.Pipe()
-Beam_flipped_2.x = Beam_flipped.x + (width/2)
+Beam_flipped_2.x = (Beam_flipped.x) + (width/2) + (pipe_width/2)
 Beam_flipped_2.y = pipe.calc_coords()
 
 Beam_2 = pipe.Pipe()
-Beam_2.x = Beam.x + (width/2)
-Beam_2.y = (Beam_flipped_2.y+pipe_height) + (bird_height*3)
+Beam_2.x = Beam_flipped_2.x
+Beam_2.y = (Beam_flipped_2.y+pipe_height) + (bird_height*4)
 
 running = True
 
 while running:
+    # Adding the Background Image
     window.blit(bg_image, (0,0))
+    # The flappy bird
     window.blit(bird_image_mid, (Flappy_bird.x, Flappy_bird.y))
     
+    # First pair of pipes (flipped and straight)
     window.blit(pipe_image_flipped, (Beam_flipped.x, Beam_flipped.y))
     window.blit(pipe_image, (Beam.x, Beam.y))
     
+    # Second pair of pipes
     window.blit(pipe_image_flipped, (Beam_flipped_2.x, Beam_flipped_2.y))
     window.blit(pipe_image, (Beam_2.x, Beam_2.y))
     
+    # Moving the pipes
     Beam_flipped.x -= 0.1
     Beam.x -= 0.1
-    
     Beam_flipped_2.x -= 0.1
     Beam_2.x -= 0.1
     
-    if Beam_flipped.x < 0:
-        Beam_flipped.reset()
-        Beam.reset()
-    elif Beam_flipped_2.x < 0:
-        Beam_flipped_2.reset()
-        Beam_2.reset()
+    # Repeating the pipes by reseting it's position
+    if Beam_flipped.x < (0 - pipe_width):
+        Beam_flipped.reset(pipe_width, width)
+        Beam.reset(pipe_width, width)
+    elif Beam_flipped_2.x < (0 - pipe_width):
+        Beam_flipped_2.reset(pipe_width, width)
+        Beam_2.reset(pipe_width, width)
     
-    # 2 beams on screen at a time
+    # Event loop
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
     
+    # Updating the display
     pg.display.update()
     
--- src/pipe.py
+++ src/pipe.py
@@ -1,13 +1,12 @@
 import random
 
 def calc_coords():
-    return random.randint(-250, 0) # 320 is height of beam so for keeping it's head, I used -250. It's for flipped beam
+    return random.randint(-250, 0) # 320 is height of beam so for keeping it's head, I used -250. It's for flipped/upper beam
 
 class Pipe:
     def __init__(self):
         self.x = 288
         self.y = 0
         
-    def reset(self):
-        self.x += 288
-        
+    def reset(self, pipe_width, width):
+        self.x = self.x + pipe_width + width
